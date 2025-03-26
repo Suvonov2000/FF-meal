b9ebcec25caf9e229b6654b4b4f5eb406535d635
Suvonov2000 <suvonov200@gmail.com>
2025-03-26 11:07:54 +0900

    FF-meal

--- src/cmponents/buyurtmalar/customs/body/menu-h/index.jsx
+++ src/cmponents/buyurtmalar/customs/body/menu-h/index.jsx
@@ -1,8 +1,12 @@
+import { Container } from "./style";
 import ProductCardH from "../productCardH";
 
 const MenuH = () => {
   return (
     <Container>
+      <ProductCardH />
+      <ProductCardH />
+      <ProductCardH />
       <ProductCardH />
     </Container>
   );
